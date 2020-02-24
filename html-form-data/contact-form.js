f0bdd07dcc740590f6b1cb3bff084bef1bdf37fe
Tayla Monet <taymjenk@gmail.com>
2020-02-24 13:20:38 -0500

    npm i mocha chai, html form notes

--- html-form-data/contact-form.js
+++ html-form-data/contact-form.js
@@ -1,3 +1,22 @@
+//handling html forms:
+
+// grab form element by document.forms with id or array idx and add event listner for a submit event, firing a handleSubmit func when event occurs
+
+//handle submit func:
+// prevents default flashing of page
+// converts form data to a JSON object --> passes array-like form.elements to formToJSON func
+// grabs the html element to display form results
+// sets text content of html display element to the stringified JSON data object
+
+//formToJSON func:
+// takes all elements within the form
+// creates array from all form elements loops over each element and performs two checks:
+// 1. is element a valid element? does it have an element.name and element.value? (ex: submit button element)
+// 2. if element is valid, is it a checkbox? (checkbox data will grab an element even if not selected). If element is valid and is a checkbox, it has a checked attribute
+// if multiple options are selected in checkbox, it will add options into an array
+//reduces valid elements/radios/checkboxes to empty object
+//returns object
+
 
 const isCheckbox = element => element.type === "checkbox"
 
@@ -18,7 +37,8 @@ const formToJSON = elements => {
   //binds [] to form.elements, forcing reduce to work with elements
   //inital value = empty obj
   //reduces each elements name and value into new obj
-  const formData = [].reduce.call(elements, (parsedDataObj, element) => {
+  console.log(elements)
+  const formData = Array.from(elements).reduce((parsedDataObj, element) => {
     if(isValidElement(element) && isValidValue(element)) {
       //checks if element is a checkbox
       //if so, adds element.name as new key in parsedDataObj
@@ -35,7 +55,7 @@ const formToJSON = elements => {
   return formData
 }
 
-const handleSubmit = event => {
+function handleSubmit(event) {
   //prevent the default submit button, stops quick flash of the page event
   event.preventDefault()
   //creates empty data object, not JSON
@@ -53,6 +73,13 @@ const handleSubmit = event => {
   dataContainer.textContent = JSON.stringify(data, null, " ")
 }
 
-const form = document.getElementsByClassName("content-form")[0]
-//console.log(form.elements)
+const form = document.forms[0]
 form.addEventListener("submit", handleSubmit)
+
+module.exports = {
+  isCheckbox,
+  isValidValue,
+  isValidElement,
+  formToJSON,
+  handleSubmit
+}
