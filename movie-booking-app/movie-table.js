c5d1dfa42320ab121d55020bd771ecc798f75081
Tayla Monet <taymjenk@gmail.com>
2020-03-08 18:52:35 -0400

    table css

--- movie-booking-app/movie-table.js
+++ movie-booking-app/movie-table.js
@@ -1,46 +1,47 @@
 const movies = [
   {
-    name: 'mad max: fury road',
-    genre: 'action',
+    name: "mad max: fury road",
+    genre: "action",
     releaseDate: 2015,
-    rating: 'R'
+    rating: "R"
   },
   {
-    name: 'moonlight',
-    genre: 'drama',
+    name: "moonlight",
+    genre: "drama",
     releaseDate: 2016,
-    rating: 'R'
+    rating: "R"
   },
   {
-    name: 'the social network',
-    genre: 'biography',
+    name: "the social network",
+    genre: "biography",
     releaseDate: 2010,
-    rating: 'PG-13'
+    rating: "PG-13"
   },
   {
-    name: 'get out',
-    genre: 'horror',
+    name: "get out",
+    genre: "horror",
     releaseDate: 2017,
-    rating: 'R'
+    rating: "R"
   },
   {
-    name: 'inception',
-    genre: 'action',
+    name: "inception",
+    genre: "action",
     releaseDate: 2010,
-    rating: 'PG-13'
+    rating: "PG-13"
   }
 ]
 //grab table html element
-let movieTable = document.querySelector('#movie-table')
+let movieTable = document.querySelector("#movie-table")
 
 //take first movie and create table header row off of obj keys
 //append first row to table
 let movieHeaders = Object.keys(movies[0])
-let tableHead = document.createElement('thead')
-tableHead.id = 'table-head'
-let headRow = document.createElement('tr')
+let tableHead = document.createElement("thead")
+tableHead.id = "table-head"
+let headRow = document.createElement("tr")
+headRow.className = "table-head-row"
 movieHeaders.forEach(header => {
-  let colTitle = document.createElement('th')
+  let colTitle = document.createElement("th")
   colTitle.innerText = header
   headRow.appendChild(colTitle)
 })
@@ -50,19 +51,20 @@ movieTable.appendChild(tableHead)
 //for each movie in movies array, create a new table row and table cells for each movie info
 //append to row, append row to table
 function insertRow(newObj) {
-  const newRow = document.createElement('tr')
+  const newRow = document.createElement("tr")
+  newRow.className = "table-row"
   for (let key in newObj) {
-    let newCell = document.createElement('td')
+    let newCell = document.createElement("td")
+    newCell.className = "table-cell"
     newCell.innerText = newObj[key]
     newRow.appendChild(newCell)
   }
   return newRow
 }
 
-let tableBody = document.createElement('tbody')
-tableBody.id = 'table-body'
+let tableBody = document.createElement("tbody")
+tableBody.id = "table-body"
 movies.forEach(movie => {
   tableBody.appendChild(insertRow(movie))
 })
 movieTable.appendChild(tableBody)
-
