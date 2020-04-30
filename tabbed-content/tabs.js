8f07358a2a7c6fc0bfd4752df0c81bec6c06a6d8
Tayla Monet <taymjenk@gmail.com>
2020-04-30 12:49:44 -0400

    replacing active class with focus-tab nav by keys

--- tabbed-content/tabs.js
+++ tabbed-content/tabs.js
@@ -9,10 +9,18 @@ function openTab(event) {
 
   //grab all tabs and remove active from className
   const tabs = tabDiv.querySelectorAll('.tab')
-  Array.from(tabs).forEach(tab => tab.className = tab.className.replace(' active',''))
+  Array.from(tabs).forEach(tab => tab.className = tab.className.replace(' focus',''))
 
   //grab target lang, set target button to active and display block according to targetLang
   const targetLanguage = event.target.name
-  event.target.className += " active"
+  event.target.className += " focus"
   document.querySelector(`#${targetLanguage}`).style.display = 'block'
 }
+
+//allows tab navigation by arrow keys
+const tabs = tabDiv.querySelectorAll('.tab')
+Array.from(tabs).forEach(tab => tab.addEventListener('keyup', function(event) {
+    if(event.keyCode === 39 && this.nextElementSibling) this.nextElementSibling.focus()
+    if(event.keyCode === 37 && this.previousElementSibling) this.previousElementSibling.focus()
+  }))
+
